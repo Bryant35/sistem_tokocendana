2fd8400dbdc15b6f92f3f6dd93235be81c18d7a1
Bryant35 <banthony@student.ciputra.ac.id>
2022-04-25 15:05:33 +0700

    fix navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="nav nav-justified">
+    <nav class="navbar nav-justified">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Toko
@@ -41,7 +41,7 @@
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
-          </li>a
+          </li>
     </nav>
 </body>
 </html>
