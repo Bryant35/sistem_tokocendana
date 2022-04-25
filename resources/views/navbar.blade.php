37418301a5d657db322f3b08274944df5f005654
Bryant35 <banthony@student.ciputra.ac.id>
2022-04-25 15:47:30 +0700

    Update Nav Justify

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,44 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar nav-justified navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar scroll</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Link
+    <div class="mx-5">
+        <nav class="navbar navbar-collapse dark mx-5">
+            <a class="nav-link" href="/"><img src="img\logo_toko_cendana.png" alt="" style="width: 60px; text-align: left;"></a>
+            <li class="nav-item dropdown " style="text-decoration: black; list-style: none;">
+                <a class="nav-item dropdown-toggle text-reset" href="#" id="navbarToko" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: black;">
+                Toko
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <ul class="dropdown-menu" aria-labelledby="navbarToko" style="text-decoration: none;">
+                    <li><a class="dropdown-item" href="#">Transaksi Jual</a></li>
+                    <li><a class="dropdown-item" href="#">Konversi Stok</a></li>
+                    <li><a class="dropdown-item" href="#">Penyesuaian Stok</a></li>
                 </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-    </nav>
+            </li>
+            <li class="nav-item dropdown" style="text-decoration: black; list-style: none; ">
+                <a class="nav-item dropdown-toggle text-reset" href="#" id="navbarGudang" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: black;">
+                Gudang
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarGudang" style="text-decoration: none;">
+                    <li><a class="dropdown-item" href="#">Produk Mentah</a></li>
+                    <li><a class="dropdown-item" href="#">Produk Jadi</a></li>
+                    <li><a class="dropdown-item" href="#">Retur Produk</a></li>
+                </ul>
+            </li>
+            <a class="nav-item text-reset" href="#" style="text-decoration: none;">Pegawai</a>
+            <a class="nav-item text-reset" href="#" style="text-decoration: none;">Customer</a>
+            <li class="nav-item dropdown" style="text-decoration: black; list-style: none; ">
+                <a class="nav-item dropdown-toggle text-reset" href="#" id="navbarLaporan" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: black;">
+                Laporan
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarLaporan" style="text-decoration: none;">
+                    <li><a class="dropdown-item" href="#">Stok Masuk-Keluar</a></li>
+                    <li><a class="dropdown-item" href="#">Penjualan</a></li>
+                    <li><a class="dropdown-item" href="#">Retur Produk</a></li>
+                    <li><a class="dropdown-item" href="#">Konversi Stok</a></li>
+                </ul>
+            </li>
+        </nav>
+    </div>
 </body>
 </html>
