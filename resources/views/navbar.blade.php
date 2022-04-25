a0a978dd2633b7f44c2a2424843cbe391e6b4ded
Bryant35 <banthony@student.ciputra.ac.id>
2022-04-25 14:52:06 +0700

    Navbar test

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,6 +7,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    
+    <nav class="nav nav-justified">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Toko
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Transaksi Jual</a></li>
+                <li><a class="dropdown-item" href="#">Konversi Stok</a></li>
+                <li><a class="dropdown-item" href="#">Penyesuaian Stok</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Gudang
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                <li><a class="dropdown-item" href="#">Produk Mentah</a></li>
+                <li><a class="dropdown-item" href="#">Produk Jadi</a></li>
+                <li><a class="dropdown-item" href="#">Retur Produk</a></li>
+            </ul>
+        </li>
+        <a class="nav-link" href="#">Pegawai</a>
+        <a class="nav-link" href="#">Customer</a>
+        <a class="nav-link" href="#">Laporan</a>
+    </nav>
 </body>
 </html>
