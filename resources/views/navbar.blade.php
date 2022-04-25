e210382e685b729f2c240d5611ad870b16dab86c
Bryant35 <banthony@student.ciputra.ac.id>
2022-04-25 15:04:10 +0700

    tes navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -31,6 +31,17 @@
         <a class="nav-link" href="#">Pegawai</a>
         <a class="nav-link" href="#">Customer</a>
         <a class="nav-link" href="#">Laporan</a>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>a
     </nav>
 </body>
 </html>
